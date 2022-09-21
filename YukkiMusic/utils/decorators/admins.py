ffbfb1ebebd3c73a0ea299a02bdc3098811b1e5a
Oniel <51058739+0niel@users.noreply.github.com>
2022-09-21 21:18:42 +0300

    Fix chat privileges check

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -92,7 +92,7 @@ def AdminActual(mystic):
                 member = await app.get_chat_member(message.chat.id, message.from_user.id)
             except Exception:
                 return
-            if not member.can_manage_video_chats:
+            if not member.privileges.can_manage_video_chats:
                 return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
@@ -126,7 +126,7 @@ def ActualAdminCB(mystic):
                 return await CallbackQuery.answer(
                     _["general_5"], show_alert=True
                 )
-            if not a.can_manage_video_chats and CallbackQuery.from_user.id not in SUDOERS:
+            if not a.privileges.can_manage_video_chats and CallbackQuery.from_user.id not in SUDOERS:
                 token = await int_to_alpha(
                     CallbackQuery.from_user.id
                 )
